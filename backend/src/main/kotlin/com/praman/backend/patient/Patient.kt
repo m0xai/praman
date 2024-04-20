bd875c8812c44ec823475fd7e0a4aa90e9bfa80c
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-20 14:10:14 +0200

    feat(backend): create Patient model with DTOs

--- backend/src/main/kotlin/com/praman/backend/patient/Patient.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/Patient.kt
@@ -1,3 +1,16 @@
 package com.praman.backend.patient
 
-data class Patient()
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.util.*
+
+@Entity(name = "patients")
+data class Patient(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long = 0,
+        val fullName: String,
+        val dateOfBirth: Date
+)
--- backend/src/main/kotlin/com/praman/backend/patient/PatientDTO.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/PatientDTO.kt
@@ -1,4 +1,36 @@
 package com.praman.backend.patient
 
-class PatientDTO {
-}
+import java.util.*
+
+data class PatientCreateRequest(
+        val fullName: String,
+        val dateOfBirth: Date
+)
+
+fun PatientCreateRequest.toEntity() = Patient(
+        fullName = fullName,
+        dateOfBirth = dateOfBirth
+)
+
+data class PatientUpdateRequest(
+        val fullName: String,
+        val dateOfBirth: Date
+)
+
+fun PatientUpdateRequest.toEntity(id: Long) = Patient(
+        id = id,
+        fullName = fullName,
+        dateOfBirth = dateOfBirth
+)
+
+data class PatientResponse(
+        val id: Long,
+        val fullName: String,
+        val dateOfBirth: Date
+)
+
+fun Patient.toResponse() = PatientResponse(
+        id = id,
+        fullName = fullName,
+        dateOfBirth = dateOfBirth
+)
