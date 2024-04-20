eda8e118998241fb599a245e20445cb9ba1cc016
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-20 14:17:47 +0200

    feat(backend): create PatientController

--- backend/src/main/kotlin/com/praman/backend/patient/PatientController.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/PatientController.kt
@@ -1,4 +1,31 @@
 package com.praman.backend.patient
 
-class PatientController {
+import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("patients")
+@CrossOrigin(origins = ["http://localhost:4200"])
+class PatientController(val service: PatientService) {
+    @GetMapping("/")
+    fun list(): ResponseEntity<List<Patient>> {
+        return ResponseEntity.ok().body(service.list())
+    }
+
+    @GetMapping("/{id}/")
+    fun get(@PathVariable id: Long): ResponseEntity<Patient> {
+        try {
+            return ResponseEntity.ok().body(service.get(id))
+        } catch (ex: ResourceNotFoundException) {
+            return ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping("/")
+    fun create(@RequestBody request: PatientCreateRequest): ResponseEntity<PatientResponse> {
+        val patient = service.create(request)
+        return ResponseEntity.ok().body(patient.toResponse())
+    }
+
 }
