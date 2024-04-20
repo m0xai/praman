f35e169ceb204d69756984c4895bdc2f96e10959
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-20 14:11:05 +0200

    feat(backend): create PatientService to apply business logic

--- backend/src/main/kotlin/com/praman/backend/patient/PatientService.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/PatientService.kt
@@ -1,4 +1,24 @@
 package com.praman.backend.patient
 
-class PatientService {
+import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class PatientService(val repository: PatientRepository) {
+    fun list(): List<Patient> {
+        return repository.findAll()
+    }
+
+    fun get(id: Long): Patient {
+        val patient = repository.findById(id);
+        if (patient.isPresent) {
+            return patient.get()
+        } else {
+            throw ResourceNotFoundException("Patient with ID: $id not found.")
+        }
+    }
+
+    fun create(request: PatientCreateRequest): Patient {
+        return repository.save(request.toEntity())
+    }
 }
