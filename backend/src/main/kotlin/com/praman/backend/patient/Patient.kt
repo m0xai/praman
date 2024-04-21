909c367c3c9ed1a6c418c579c6e42bdaebad130d
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-21 12:29:57 +0200

    refactor(backend): use base entity for patient model

--- backend/src/main/kotlin/com/praman/backend/patient/Patient.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/Patient.kt
@@ -1,16 +1,15 @@
 package com.praman.backend.patient
 
+import com.praman.backend.base.BaseEntity
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import java.util.*
 
 @Entity(name = "patients")
 data class Patient(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        var id: Long = 0,
-        val fullName: String,
-        val dateOfBirth: Date
-)
+    val fullName: String,
+    val dateOfBirth: Date
+) : BaseEntity() {
+    constructor(id: Long, fullName: String, dateOfBirth: Date) : this(fullName, dateOfBirth) {
+        this.id = id
+    }
+}
