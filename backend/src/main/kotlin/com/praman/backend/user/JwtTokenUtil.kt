937a28cc066ffe6b413441d730b5a65b1ae92d2a
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-24 20:28:16 +0200

    refactor(backend): update JwtTokenUtil to apply changed jjwt methods

--- backend/src/main/kotlin/com/praman/backend/user/JwtTokenUtil.kt
+++ backend/src/main/kotlin/com/praman/backend/user/JwtTokenUtil.kt
@@ -1,4 +1,31 @@
 package com.praman.backend.user
 
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
 class JwtTokenUtil {
+
+    private val secret = "YOUR_SECRET"
+    private val expiration = 6000000
+
+    fun generateToken(username: String): String =
+        Jwts.builder().setSubject(username).setExpiration(Date(System.currentTimeMillis() + expiration))
+            .signWith(SignatureAlgorithm.HS512, secret).compact()
+
+    private fun getClaims(token: String): Claims = Jwts.parser().setSigningKey(secret).build().parseSignedClaims(token)
+        .getPayload();
+
+    fun getEmail(token: String): String = getClaims(token).subject
+
+    fun isTokenValid(token: String): Boolean {
+        val claims = getClaims(token)
+        val expirationDate = claims.expiration
+        val now = Date(System.currentTimeMillis())
+        return now.before(expirationDate)
+    }
 }
+
