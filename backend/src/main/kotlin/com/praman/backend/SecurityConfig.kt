b258575d102cf9989f0d304dc6979ce56096ff54
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-25 13:32:51 +0200

    chore(frontend): remove date-fns, and use dayjs, since antd uses this out-of-box

--- backend/src/main/kotlin/com/praman/backend/SecurityConfig.kt
+++ backend/src/main/kotlin/com/praman/backend/SecurityConfig.kt
@@ -57,6 +57,7 @@ class SecurityConfig(
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
             HttpMethod.PUT.name(),
+            HttpMethod.PATCH.name(),
             HttpMethod.DELETE.name(),
             HttpMethod.OPTIONS.name()
         )
