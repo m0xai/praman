fb76792987b7a1512c9a74de5cd312961615aeea
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-25 13:00:19 +0200

    feat(frontend): show phone number and blood group in patinet list

--- frontend/src/pages/categories/edit.tsx
+++ frontend/src/pages/categories/edit.tsx
@@ -1,9 +1,19 @@
 import { Edit, useForm } from "@refinedev/antd";
-import { DatePicker, Form, Input } from "antd";
+import { DatePicker, Form, Input, Select } from "antd";
 import dayjs from "dayjs";
+import { BloodGroup } from "../../schemas/BloodGroup";
 
 export const CategoryEdit = () => {
   const { formProps, saveButtonProps } = useForm({});
+
+  const bloodGrpupKeys = Object.keys(BloodGroup).filter(
+    (key: string | number) => (isNaN(Number(key)) ? true : false)
+  );
+
+  const bloodGrpupOptions = bloodGrpupKeys.map((key: string) => ({
+    label: BloodGroup[key],
+    value: key,
+  }));
 
   return (
     <Edit saveButtonProps={saveButtonProps}>
@@ -30,6 +40,20 @@ export const CategoryEdit = () => {
           getValueProps={(val) => ({ value: val ? dayjs(val) : "" })}
         >
           <DatePicker />
+        </Form.Item>
+        <Form.Item
+          label={"Phone Number"}
+          name={["phoneNumber"]}
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label={"Blood Group"}
+          name={["bloodGroup"]}
+          rules={[{ required: true }]}
+        >
+          <Select options={bloodGrpupOptions} />
         </Form.Item>
       </Form>
     </Edit>
