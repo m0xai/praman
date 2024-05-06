389df70496e23a71da810683f12c4ecb2f4dae3c
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-06 22:08:10 +0200

    feat(backend): create app user data class with it's init fields

--- backend/src/main/kotlin/com/praman/backend/user/AppUser.kt
+++ backend/src/main/kotlin/com/praman/backend/user/AppUser.kt
@@ -1,4 +1,21 @@
 package com.praman.backend.user
 
-class AppUser {
+import com.praman.backend.base.BaseEntity
+import jakarta.persistence.Entity
+
+@Entity
+class AppUser(
+    val username: String,
+    val password: String,
+    val email: String,
+    val role: String
+) : BaseEntity() {
+    constructor(id: Long, username: String, password: String, email: String, role: String) : this(
+        username,
+        password,
+        email,
+        role
+    ) {
+        this.id = id
+    }
 }
