de54620e5287b9f4e9b81f2ed3ee4bd4cc35e9d0
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-07 22:08:05 +0200

    feat(backend): create jwtauthentication filter

--- backend/src/main/kotlin/com/praman/backend/user/JwtAuthenticationFilter.kt
+++ backend/src/main/kotlin/com/praman/backend/user/JwtAuthenticationFilter.kt
@@ -1,4 +1,62 @@
 package com.praman.backend.user
 
-class JwtAuthenticationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import java.util.*
+
+class JwtAuthenticationFilter(
+    private val jwtTokenUtil: JwtTokenUtil,
+    private val authenticationManager: AuthenticationManager
+) :
+    UsernamePasswordAuthenticationFilter() {
+
+    override fun attemptAuthentication(req: HttpServletRequest, response: HttpServletResponse): Authentication {
+        val credentials = ObjectMapper().readValue(req.inputStream, LoginDTO::class.java)
+        val auth = UsernamePasswordAuthenticationToken(
+            credentials.email,
+            credentials.password,
+            Collections.singleton(SimpleGrantedAuthority("user"))
+        )
+        return authenticationManager.authenticate(auth)
+    }
+
+    override fun successfulAuthentication(
+        req: HttpServletRequest?, res: HttpServletResponse, chain: FilterChain?,
+        auth: Authentication
+    ) {
+        val username = (auth.principal as AppUserDetails).username
+        val token: String = jwtTokenUtil.generateToken(username)
+        res.addHeader("Authorization", token)
+        res.addHeader("Access-Control-Expose-Headers", "Authorization")
+    }
+
+    override fun unsuccessfulAuthentication(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        failed: AuthenticationException
+    ) {
+        val error = BadCredentialsError()
+        response.status = error.status
+        response.contentType = "application/json"
+        response.writer.append(error.toString())
+    }
+
+}
+
+private data class BadCredentialsError(
+    val timestamp: Long = Date().time,
+    val status: Int = 401,
+    val message: String = "Email or password incorrect",
+) {
+    override fun toString(): String {
+        return ObjectMapper().writeValueAsString(this)
+    }
 }
