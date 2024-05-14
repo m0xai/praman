745ba2c7dce03d461061637cb6badef2e62f2bdb
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-14 22:43:41 +0200

    feat(backend): create AppUserService to implement custom UserDetailsService

--- backend/src/main/kotlin/com/praman/backend/user/AppUserService.kt
+++ backend/src/main/kotlin/com/praman/backend/user/AppUserService.kt
@@ -1,4 +1,33 @@
 package com.praman.backend.user
 
-class AppUserService {
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+import java.util.*
+
+
+@Service
+class AppUserService(
+    private val repository: AppUserRepository,
+) : UserDetailsService {
+    override fun loadUserByUsername(email: String): UserDetails {
+        // Create a method in your repo to find a user by its username
+        val user = repository.findByEmail(email)
+        if (user.isPresent) {
+            val u = user.get()
+            return AppUserDetails(
+                u.id,
+                u.email,
+                u.password,
+                Collections.singleton(SimpleGrantedAuthority("ROLE_" + u.role))
+            )
+        }
+        return AppUserDetails(
+            0,
+            "",
+            "",
+            Collections.singleton(SimpleGrantedAuthority("user"))
+        )
+    }
 }
