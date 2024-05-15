a505a03afd697fe368ebbc6dea7fb80ca63586ef
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-15 21:36:14 +0200

    feat(backend): make email field of patient non-nullable and unique

--- backend/src/main/kotlin/com/praman/backend/patient/Patient.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/Patient.kt
@@ -2,11 +2,13 @@ package com.praman.backend.patient
 
 import com.praman.backend.base.BaseEntity
 import com.praman.backend.patient.enums.BloodGroup
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import java.util.*
 
 @Entity(name = "patients")
 data class Patient(
+    @Column(unique = true, nullable = false)
     val email: String,
     val fullName: String,
     val dateOfBirth: Date,
