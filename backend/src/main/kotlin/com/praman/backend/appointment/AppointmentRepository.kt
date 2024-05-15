496786972466f5e5dba0c4ebde14c9854f2ff806
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-15 22:00:38 +0200

    feat(backend): add findAllByDoctorEmail function

--- backend/src/main/kotlin/com/praman/backend/appointment/AppointmentRepository.kt
+++ backend/src/main/kotlin/com/praman/backend/appointment/AppointmentRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository
 @Repository
 interface AppointmentRepository : JpaRepository<Appointment, Long> {
     fun findAllByPatientEmail(email: String): List<Appointment>
+    fun findAllByDoctorEmail(doctorEmail: String): List<Appointment>
 }
