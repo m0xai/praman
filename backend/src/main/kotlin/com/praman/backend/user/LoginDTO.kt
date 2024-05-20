5b66be2441912f614ca28a5813a7d2e5e7494d56
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-20 16:39:59 +0200

    feat(backend): create loginDTO to read values from login API call

--- backend/src/main/kotlin/com/praman/backend/user/LoginDTO.kt
+++ backend/src/main/kotlin/com/praman/backend/user/LoginDTO.kt
@@ -1,4 +1,9 @@
 package com.praman.backend.user
 
-class LoginDTO {
-}
+import java.beans.ConstructorProperties
+
+data class LoginDTO
+@ConstructorProperties("email", "password")
+constructor(val email: String, val password: String)
+
+data class LoginResponse(val token: String)
