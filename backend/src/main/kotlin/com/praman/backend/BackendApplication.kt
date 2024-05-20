4288927540fa2fa60f41d8e00764a2df7719c5f9
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-20 16:40:44 +0200

    feat(backend): set initial admin, doctor and patient users temporarily

--- backend/src/main/kotlin/com/praman/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/praman/backend/BackendApplication.kt
@@ -1,11 +1,43 @@
 package com.praman.backend
 
+import com.praman.backend.user.AppUser
+import com.praman.backend.user.AppUserRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
     runApplication<BackendApplication>(*args)
+}
+
+
+@Component
+class PraManInit(
+    private val repo: AppUserRepository,
+    private val passwordEncoder: BCryptPasswordEncoder
+) {
+    init {
+        createUser()
+    }
+
+    fun createUser() {
+        val u = repo.findByEmail("kerem@z.de")
+        if (!u.isPresent)
+            repo.save(
+                AppUser(
+                    1, "kerem", password = passwordEncoder.encode("1299"), email = "kerem@z.de", role =
+                    "ADMIN"
+                ),
+            )
+        repo.save(
+            AppUser(2, "patient", password = passwordEncoder.encode("1299"), email = "pat@ie.nt", "PATIENT")
+        )
+        repo.save(
+            AppUser(3, "doctor", password = passwordEncoder.encode("1299"), email = "do@ct.or", "DOCTOR")
+        )
+    }
 }
