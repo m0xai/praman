239d3f7f6a244420f2e7e86cec4478b5830bc818
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-20 16:41:14 +0200

    feat(backend): annotate PatientRepository with Repository annotation

--- backend/src/main/kotlin/com/praman/backend/patient/PatientRepository.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/PatientRepository.kt
@@ -1,7 +1,8 @@
 package com.praman.backend.patient
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-
-interface PatientRepository: JpaRepository<Patient, Long>{
+@Repository
+interface PatientRepository : JpaRepository<Patient, Long> {
 }
