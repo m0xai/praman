af542e31c33c2675c77e4cc4ce31576e324dddea
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-05-29 23:28:06 +0200

    style(backend): apply styling rules

--- backend/src/main/kotlin/com/praman/backend/patient/PatientService.kt
+++ backend/src/main/kotlin/com/praman/backend/patient/PatientService.kt
@@ -5,42 +5,42 @@ import org.springframework.stereotype.Service
 
 @Service
 class PatientService(val repository: PatientRepository) {
-  fun getEntityById(id: Long): Patient {
-    if (repository.existsById(id)) {
-      return repository.findById(id).get()
+    fun getEntityById(id: Long): Patient {
+        if (repository.existsById(id)) {
+            return repository.findById(id).get()
+        }
+        throw ResourceNotFoundException("Patient with ID: $id not found.")
     }
-    throw ResourceNotFoundException("Patient with ID: $id not found.")
-  }
 
-  fun list(): List<PatientResponse> {
-    return repository.findAll().map { it.toResponse() }
-  }
-
-  fun get(id: Long): PatientResponse {
-    if (repository.existsById(id)) {
-      return repository.findById(id).get().toResponse()
-    } else {
-      throw ResourceNotFoundException("Patient with ID: $id not found.")
+    fun list(): List<PatientResponse> {
+        return repository.findAll().map { it.toResponse() }
     }
-  }
 
-  fun create(request: PatientCreateRequest): Patient {
-    return repository.save(request.toEntity())
-  }
-
-  fun update(id: Long, request: PatientUpdateRequest): Patient {
-    if (repository.existsById(id)) {
-      return repository.save(request.toEntity(id))
-    } else {
-      throw ResourceNotFoundException("Patient with ID: $id not found.")
+    fun get(id: Long): PatientResponse {
+        if (repository.existsById(id)) {
+            return repository.findById(id).get().toResponse()
+        } else {
+            throw ResourceNotFoundException("Patient with ID: $id not found.")
+        }
     }
-  }
 
-  fun delete(id: Long) {
-    if (repository.existsById(id)) {
-      return repository.deleteById(id)
-    } else {
-      throw ResourceNotFoundException("Patient with ID: $id not found.")
+    fun create(request: PatientCreateRequest): Patient {
+        return repository.save(request.toEntity())
     }
-  }
+
+    fun update(id: Long, request: PatientUpdateRequest): Patient {
+        if (repository.existsById(id)) {
+            return repository.save(request.toEntity(id))
+        } else {
+            throw ResourceNotFoundException("Patient with ID: $id not found.")
+        }
+    }
+
+    fun delete(id: Long) {
+        if (repository.existsById(id)) {
+            return repository.deleteById(id)
+        } else {
+            throw ResourceNotFoundException("Patient with ID: $id not found.")
+        }
+    }
 }
