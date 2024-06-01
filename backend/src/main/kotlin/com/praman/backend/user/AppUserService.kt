1e8a47302ad84dcb43cac7ca5acbe20808458416
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-06-01 20:45:06 +0200

    feat(backend): add getUserByEmail into user service for doctor and patient users

--- backend/src/main/kotlin/com/praman/backend/user/AppUserService.kt
+++ backend/src/main/kotlin/com/praman/backend/user/AppUserService.kt
@@ -1,5 +1,8 @@
 package com.praman.backend.user
 
+import com.praman.backend.doctor.DoctorService
+import com.praman.backend.patient.PatientService
+import com.praman.backend.patient.exceptions.ResourceNotFoundException
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -10,6 +13,8 @@ import java.util.*
 @Service
 class AppUserService(
     private val repository: AppUserRepository,
+    private val doctorService: DoctorService,
+    private val patientService: PatientService
 ) : UserDetailsService {
     override fun loadUserByUsername(email: String): UserDetails {
         // Create a method in your repo to find a user by its username
@@ -29,5 +34,21 @@ class AppUserService(
             "",
             Collections.singleton(SimpleGrantedAuthority("user"))
         )
+    }
+
+    fun getByEmail(email: String): Any {
+        val oUser = repository.findByEmail(email);
+        if (oUser.isEmpty) {
+            throw ResourceNotFoundException("User with the email: ${email} not found")
+        }
+        val user = oUser.get();
+        if (user.role == "ROLE_DOCTOR") {
+            val doctor = doctorService.getByEmail(user.email)
+            return DoctorUser(user.id, user.email, user.username, user.role, doctor)
+        } else if (user.role == "ROLE_PATIENT") {
+            val patient = patientService.getByEmail(user.email)
+            return PatientUser(user.id, user.email, user.username, user.role, patient)
+        }
+        throw ResourceNotFoundException("User with the email: ${user.email} not found")
     }
 }
